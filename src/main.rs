4df25d6456b0b2b0d70faa01ea5c0900edaf9b84
Richard <cod2colt@gmail.com>
2026-01-06 22:30:27 +0800

    add gui with egui
    add gui interface with egui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+// my rust egui
+
+use eframe::egui;
+
+fn main() -> eframe::Result {
+    // set initialize windows size
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default(),
+        ..Default::default()
+    };
+    // create my app
+    eframe::run_native(
+        "My Rust egui app",
+        options,
+        Box::new(|cc| Ok(Box::<MyApp>::default())),
+    )
+}
+
+// create My app structure
+struct MyApp {
+    name: String,
+}
+
+// impl methods fro my app
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Hello Rust egui".to_owned(),
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        // my egui layout
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // add label
+            ui.heading("My Rust egui");
+            // a input
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+                ui.heading(&self.name);
+            });
+        });
+    }
 }
