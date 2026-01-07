ee854f0540d4e7fcff0b9b099dbb94a1c18b46a0
Richard <cod2colt@gmail.com>
2026-01-08 00:24:36 +0800

    add thread and timer
    add thread and timer demo

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod customfonts;
 
 use eframe::egui;
 use egui::{Button, RichText};
+use std::sync::mpsc::{self, Receiver, Sender};
+use std::time::{Duration, Instant};
 
 // my rust
 mod myrust;
@@ -25,10 +27,13 @@ fn main() -> eframe::Result {
 
 // create My app structure
 struct MyApp {
+    rx: Receiver<()>,
+    tx: Sender<()>,
     data_1: String,
     data_2: String,
     output_buffer: String,
     run: bool,
+    counter: f64,
 }
 
 // impl methods fro my app
@@ -36,29 +41,78 @@ impl MyApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // set custom font
         customfonts::setup_custom_fonts(&cc.egui_ctx);
+
+        // thread
+        let (tx, rx) = mpsc::channel();
+        let tx_thread = tx.clone();
+        let ctx = cc.egui_ctx.clone();
+
+        // timer thread, trigger per 0.1s
+        std::thread::spawn(move || {
+            // a fixed time interval to call the thread
+            let interval = Duration::from_secs_f64(0.1);
+            // get instant now
+            let mut next_tick = Instant::now();
+            // loop
+            loop {
+                // get the next thread wake up time
+                next_tick += interval;
+                // trigger event
+                let _ = tx_thread.send(());
+                // repaint
+                ctx.request_repaint();
+                // check the remain time
+                let now = Instant::now();
+                if next_tick > now {
+                    // normal process when we have enough time
+                    std::thread::sleep(next_tick - now);
+                } else {
+                    // behind, reset the time, avoid dead lock
+                    if now - next_tick > Duration::from_secs(1) {
+                        next_tick = now;
+                    }
+                }
+            }
+        });
+
         // default data
         Self {
+            rx,
+            tx,
             data_1: "Hello".to_owned(),
             data_2: "World".to_owned(),
             output_buffer: "Hello Rust World".to_owned(),
             run: false,
+            counter: 0.0,
         }
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // run function
         if self.run {
             self.run = false;
             // my rust
             self.output_buffer.clear();
             self.output_buffer = my_rust(&self.data_1, &self.data_2);
         }
+        // timer trigger per 0.1 seconds
+        if let Ok(_) = self.rx.try_recv() {
+            self.counter += 0.1;
+        }
 
         // my egui layout
         egui::CentralPanel::default().show(ctx, |ui| {
             // add label
-            ui.heading("My Rust egui");
+            ui.vertical_centered(|ui| {
+                ui.heading("My Rust egui");
+            });
+            ui.add_space(20.0);
+            // show counter
+            let str_output = format!("Counter: {:.1}", self.counter / 10.0);
+            ui.label(str_output);
+
             ui.add_space(20.0);
 
             // input
