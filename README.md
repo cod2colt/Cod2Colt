71ac5a064ea3545551c2f03c448d2c9959f46318
richard <richardrslin@gmail.com>
2026-01-09 13:16:56 +0800

    add structure of my print
    build my print to display output data

--- README.md
+++ README.md
@@ -8,7 +8,16 @@
 - 🌱 Here's a code farmer's rebirth from Cod to Colt
 
 ## Development Environment
-This project is developed on
-- macOS 26.2
-- Rust 1.92 
-- VSCode  
+[![Rust](https://img.shields.io/badge/Rust-1.92-black?style=for-the-badge&logo=rust&logoColor=white)](https://www.rust-lang.org/)
+[![VSCode](https://img.shields.io/badge/VSCode-1.107.1-blue?style=for-the-badge&logo=visual-studio-code&logoColor=white)](https://code.visualstudio.com/)
+[![macOS](https://img.shields.io/badge/macOS-26.2-lightgrey?style=for-the-badge&logo=apple&logoColor=black)](https://www.apple.com/macos/)
+
+
+## Github
+[![GitHub](https://img.shields.io/badge/GitHub-cod2colt-black?style=for-the-badge&logo=github&logoColor=white)](https://github.com/cod2colt)  
+[![GitHub Repo](https://img.shields.io/badge/Repository-Cod2Colt-blue?style=for-the-badge&logo=github&logoColor=white)](https://github.com/cod2colt/Cod2Colt) 
+
+## Footage
+
+[![YouTube](https://img.shields.io/badge/YouTube-Cod2Colt-red?style=for-the-badge&logo=youtube&logoColor=white)](https://www.youtube.com/@Cod2Colt) 
+
--- src/myrust.rs
+++ src/myrust.rs
@@ -1,8 +1,39 @@
+// my print
+pub struct MyPrint {
+    value: Vec<String>,
+}
+
+#[allow(dead_code)]
+impl MyPrint {
+    // new a MyPrint
+    pub fn new() -> Self {
+        Self { value: Vec::new() }
+    }
+    // get value
+    pub fn get(&self) -> String {
+        self.value.concat()
+    }
+    // get value, and destroy/drop my print, release my print
+    pub fn flush(self) -> String {
+        self.value.into_iter().collect()
+    }
+    // print a line
+    pub fn print_line<S: Into<String>>(&mut self, s: S) {
+        let mut line = s.into();
+        line.push('\n');
+        self.value.push(line);
+    }
+    // append
+    pub fn print<S: Into<String>>(&mut self, s: S) {
+        self.value.push(s.into());
+    }
+}
+
 // my rust
 pub fn my_rust(data_1: &str, data_2: &str) -> String {
-    let mut str_output = String::from("");
-    str_output.push_str(data_1);
-    str_output.push_str(" ");
-    str_output.push_str(data_2);
-    str_output
+    let mut my_print = MyPrint::new();
+    my_print.print(data_1);
+    my_print.print(" ");
+    my_print.print(data_2);
+    my_print.flush()
 }
